bdcdc89a29295186b21d49d7ed1f04de13457e87
TSKsmiley <arnarfreyr29@gmail.com>
2024-02-22 08:50:47 +0100

    added svelte-purify

--- src/components/Mermaid.svelte
+++ src/components/Mermaid.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import mermaid from "mermaid";
 	import { onMount } from "svelte";
+	import { Render } from "svelte-purify/browser-only";
 
 	export let input: string;
 
-	onMount(async () => {
+	onMount(() => {
 		mermaid.initialize({ startOnLoad: true });
 	});
 
-	async function graphDefinitionToSVG(graphDefinition: any) {
+	async function graphDefinitionToSVG(graphDefinition: string) {
 		// This is to display the "Loading..." message before Mermaid hogs the main thread
 		await new Promise((resolve) => setTimeout(resolve, 0));
 		return (await mermaid.render("container", graphDefinition)).svg;
@@ -18,7 +19,7 @@
 {#await graphDefinitionToSVG(input)}
 	<p>Loading...</p>
 {:then svg}
-	{@html svg}
+	<Render html={svg} config={{ ADD_TAGS: ["foreignObject"] }}></Render>
 {:catch error}
 	<p>{error}</p>
 {/await}
