3cee388d8a953be6ce15348d880df48e7de56eb4
NybyDK <nyby.dev@gmail.com>
2024-02-26 12:19:49 +0100

    Initial attempt at drag and drop

---
+++ src/lib/components/ToolboxItem.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { NodeType } from "$lib/interfaces/network";
+	export let text: string;
+	export let type: NodeType;
+
+	function dragStart(event: DragEvent) {
+		event.dataTransfer?.setData("text/plain", type.toString());
+	}
+</script>
+
+<div draggable="true" on:dragstart={dragStart} role="button" tabindex="-1">
+	{text}
+</div>
+
+<style>
+	div {
+		border: 2px solid black;
+		padding: 5px;
+		user-select: none;
+	}
+</style>
