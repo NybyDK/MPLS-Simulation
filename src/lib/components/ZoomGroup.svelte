a02b7bd6e99bf895f65b331b68ac822a92ed8c86
JakobTopholt <jtje20@student.aau.dk>
2024-03-20 14:30:07 +0100

    Initial commit
    Not done and doesn't work

---
+++ src/lib/components/ZoomGroup.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<div class="zoomButtons">
+  <p>Xd</p>
+</div>
+
+<style>
+  .zoomButtons {
+    border: 2px solid black;
+    padding: 5px;
+    margin: 2.5px;
+    user-select: none;
+    font-size: 16px;
+  }
+</style>
