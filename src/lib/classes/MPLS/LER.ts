26deef2e190c12dcc5136ee28b88c95f6ead02d3
JakobTopholt <jtje20@student.aau.dk>
2024-04-05 09:39:30 +0200

    Clean slate
    I went back because I could for the love of God (and salted chips) not figure out what cause the tests to fail. So a clean slate and another try seemed apt resolution.

--- src/lib/classes/MPLS/LER.ts
+++ src/lib/classes/MPLS/LER.ts
@@ -2,98 +2,47 @@ import { network } from "$lib/stores/network";
 import Router from "$lib/classes/MPLS/Router";
 import type Packet from "$lib/classes/MPLS/Packet";
 import CE from "$lib/classes/MPLS/CE";
+import LIB from "$lib/classes/MPLS/LIB";
+import FIB from "$lib/classes/MPLS/FIB";
 
 export default class LER extends Router {
-  // Maps Customer Edge address to next hop and first label
-  FIB: Map<string, { label: number; nextHop: string }> = new Map();
-  // Maps incoming label to outgoing label and next hop
-  LIB: Map<number, { outgoingLabel: number; nextHop: string }> = new Map();
+  FIB = new FIB();
+  LIB = new LIB();
 
-  addEmptyFIBEntry = () => {
-    this.FIB.set("0", { label: 0, nextHop: "0" });
-  };
-
-  addEmptyLIBEntry = () => {
-    this.LIB.set(0, { outgoingLabel: 0, nextHop: "0" });
-  };
-
-  updateFIBAddress = (oldAddress: string, newAddress: string) => {
-    const oldValue = this.FIB.get(oldAddress);
-    if (!oldValue)
-      throw new Error(`Unable to update old FIB address '${oldAddress}' to '${newAddress}'.`);
-
-    this.FIB.set(newAddress, oldValue);
-    this.FIB.delete(oldAddress);
-  };
-
-  updateLIBLabel = (oldLabel: number, newLabel: number) => {
-    const oldValue = this.LIB.get(oldLabel);
-    if (!oldValue)
-      throw new Error(`Unable to update old LIB label '${oldLabel}' to '${newLabel}'.`);
-
-    this.LIB.set(newLabel, oldValue);
-    this.LIB.delete(oldLabel);
-  };
-
-  deleteFIBEntry = (address: string) => {
-    this.FIB.delete(address);
-  };
-
-  deleteLIBEntry = (label: number) => {
-    this.LIB.delete(label);
-  };
-
-  receiveFIBEntry = (destination: string, label: number, nextHop: string) => {
-    this.FIB.set(destination, { label, nextHop });
-  };
-
-  receiveLIBEntry = (incomingLabel: number, outgoingLabel: number, nextHop: string) => {
-    this.LIB.set(incomingLabel, { outgoingLabel, nextHop });
-  };
-
+  // TODO: Instead of early return, do fallback to normal routing lookup, and if that fails too, packet.drop();
   receivePacket(packet: Packet): void {
-    // Extract destination address from the packet
     const destination = packet.destination.address;
-  
-    // If the packet's label is -1, indicating it's not labeled
+
     if (packet.label === -1) {
-      // Attempt to retrieve next hop information from the Forwarding Information Base (FIB)
       const nextHop = this.FIB.get(destination)?.nextHop;
-      if (nextHop) { // If next hop is found, attempt to retrieve label information from FIB
-        const newLabel = this.FIB.get(destination)?.label;
-        if (newLabel) { // update packet's label and node information
-          packet.label = newLabel;
-          packet.node = { x: this.node.x, y: this.node.y };
-  
-          // Retrieve next router based on next hop information
-          const nextRouter = network.getRouter(parseInt(nextHop));
-          if (nextRouter) { // If next router is found, assign it as the next hop for the packet
-            packet.nextHop = nextRouter;
-          }
-        }
-      }
+      if (!nextHop) return;
+
+      const newLabel = this.FIB.get(destination)?.label;
+      if (!newLabel) return;
+
+      packet.label = newLabel;
+
+      const nextRouter = network.getRouter(parseInt(nextHop));
+      if (!nextRouter) return;
+
+      packet.nextHop = nextRouter;
     } else {
-      // else the packet must be labelled and we retrieve hop information from the LIB
       const nextHop = this.LIB.get(packet.label)?.nextHop;
-      if (nextHop) { // If next hop is found, retrieve outgoing label information from LIB
-        const newLabel = this.LIB.get(packet.label)?.outgoingLabel;
-        if (newLabel) { // If outgoing info = found, update packet's label and node information
-          packet.label = newLabel;
-          packet.node = { x: this.node.x, y: this.node.y };
-  
-          // LDP needs to communicate first hop to source CE, so it can start by sending to the correct LER
-          // Find the next router which is a CE and matches the next hop address
-          const nextRouter = network.routers.find(
-            (router) => router instanceof CE && router.address === nextHop,
-          );
-          if (nextRouter) {  // If next router is found, assign it as the next hop for the packet
-            packet.nextHop = nextRouter;
-          }
-        }
-      }
+      if (!nextHop) return;
+
+      const newLabel = this.LIB.get(packet.label)?.outgoingLabel;
+      if (!newLabel) return;
+      packet.label = newLabel;
+
+      // LDP needs to communicate firstHop to source CE, so it can start by sending to the correct LER
+      const nextRouter = network.routers.find(
+        (router) => router instanceof CE && router.address === nextHop,
+      );
+      if (!nextRouter) return;
+
+      packet.nextHop = nextRouter;
     }
-  
-    // Check if packet's TTL is below 0, if so, it will be dropped
+
     packet.decrementTTL();
   }
 
--- src/lib/classes/MPLS/LSR.ts
+++ src/lib/classes/MPLS/LSR.ts
@@ -1,79 +1,26 @@
 import { network } from "$lib/stores/network";
 import Router from "$lib/classes/MPLS/Router";
 import type Packet from "$lib/classes/MPLS/Packet";
+import LIB from "$lib/classes/MPLS/LIB";
 
 export default class LSR extends Router {
-  // Maps Customer Edge address to next hop and first label
-  FIB: Map<string, { label: number; nextHop: string }> = new Map();
-  // Maps incoming label to outgoing label and next hop
-  LIB: Map<number, { outgoingLabel: number; nextHop: string }> = new Map();
+  LIB = new LIB();
 
-  addEmptyLIBEntry = () => {
-    this.LIB.set(0, { outgoingLabel: 0, nextHop: "0" });
-  };
-
-  updateLIBLabel = (oldLabel: number, newLabel: number) => {
-    const oldValue = this.LIB.get(oldLabel);
-    if (!oldValue)
-      throw new Error(`Unable to update old LIB label '${oldLabel}' to '${newLabel}'.`);
-
-    this.LIB.set(newLabel, oldValue);
-    this.LIB.delete(oldLabel);
-  };
-
-  deleteLIBEntry = (label: number) => {
-    this.LIB.delete(label);
-  };
-
-  receiveLIBEntry = (incomingLabel: number, outgoingLabel: number, nextHop: string) => {
-    this.LIB.set(incomingLabel, { outgoingLabel, nextHop });
-  };
-
-  receivePacket(packet: Packet): void { //In short: recieve packet, see if the label is "readable", if not do normal routing.
+  // TODO: Instead of early return, do fallback to normal routing lookup, and if that fails too, packet.drop();
+  receivePacket(packet: Packet): void {
     const nextHop = this.LIB.get(packet.label)?.nextHop;
+    if (!nextHop) return;
+
     const newLabel = this.LIB.get(packet.label)?.outgoingLabel;
-  
-    // If either next hop or outgoing label is missing, fallback to normal routing
-    if (!nextHop || !newLabel) {       
-      const destination = packet.destination.address;
-  
-      // Attempt to retrieve next hop information from FIB
-      const fallbackNextHop = this.FIB.get(destination)?.nextHop;
-  
-      // If fallback next hop information is available, proceed with normal routing
-      if (fallbackNextHop) {
-        // Attempt to retrieve label information for the fallback next hop from the FIB
-        const fallbackNewLabel = this.FIB.get(destination)?.label;
-  
-        // If label information for the fallback next hop is available, update the packet's label
-        if (fallbackNewLabel) {
-          packet.label = fallbackNewLabel;
-  
-          // Retrieve the next router based on the fallback next hop information
-          const nextRouter = network.getRouter(parseInt(fallbackNextHop));
-  
-          // If the next router is found, assign it as the next hop for the packet
-          if (nextRouter) {
-            packet.nextHop = nextRouter;
-          }
-        }
-      }
-    } else { // If next hop and outgoing label are both present, we finna MPLS (OG code)
-      packet.label = newLabel;
-  
-      // Retrieve the next router based on the next hop information
-      const nextRouter = network.getRouter(parseInt(nextHop));
-  
-      // If the next router is found, assign it as the next hop for the packet
-      if (nextRouter) {
-        packet.nextHop = nextRouter;
-      }
-    }
-  
-    // Decrease the Time-to-Live (TTL) of the packet
+    if (!newLabel) return;
+    packet.label = newLabel;
+
+    const nextRouter = network.getRouter(parseInt(nextHop));
+    if (!nextRouter) return;
+    packet.nextHop = nextRouter;
+
     packet.decrementTTL();
   }
-  
 
   get type(): "LSR" {
     return "LSR";
