9454085333f212eccf6454131cf83d074deb83e8
JakobTopholt <jtje20@student.aau.dk>
2024-04-17 12:57:14 +0200

    Update HelpButton.svelte
    Added the 1,2,3 function to the helper button, and some break lines so it doesn't appear as quite as big a chunk of text.

--- src/lib/components/HelpButton.svelte
+++ src/lib/components/HelpButton.svelte
@@ -15,15 +15,18 @@
 </div>
 
 <Dialog bind:dialog>
-  <p>Drag and drop routers to add them to the network.</p>
+  <p>Drag and drop routers to add them to the network.</p><br>
   <p>
     Instead of drag and drop, you can also click and hold shift while continuously clicking to add
     many routers.
-  </p>
-  <p>Hold the shift key and drag routers to make links.</p>
-  <p>Hold the ctrl key and drag CE's to add destinations.</p>
-  <p>Double click a router or link to configure it.</p>
-  <p>The clear button will delete the entire network.</p>
+  </p><br>
+  <p>
+    Pressing 1, 2 or 3 will repsectively select CE, LER or LSR, this works with the holding of shift to insert routers.
+  </p><br>
+  <p>Hold the shift key and drag routers to make links.</p> <br>
+  <p>Hold the ctrl key and drag CE's to add destinations.</p> <br>
+  <p>Double click a router or link to configure it.</p> <br> 
+  <p>The clear button will delete the entire network.</p> <br>  
   <p>The export button will download your network as a .json file</p>
   <br />
   <p>Press Esc to close this popup.</p>
