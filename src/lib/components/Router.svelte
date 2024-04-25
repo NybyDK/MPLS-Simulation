82595cacd40617099242273fe6d7b5d81cd00f71
JakobTopholt <jtje20@student.aau.dk>
2024-04-25 09:58:08 +0200

    image to svg
    image to svg so I can checkout a previous version and clear my head

--- src/lib/components/Router.svelte
+++ src/lib/components/Router.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import type Router from "$lib/classes/MPLS/Router";
+  import RouterSymbol from "$lib/components/RouterSymbol.svelte";
 
   export let router: Router;
 </script>
 
-<image
+<svg
   id={router.id.toString()}
   x={router.node.x - 20}
   y={router.node.y - 20}
@@ -12,6 +13,7 @@
   height="45"
   href={`/${router.type}Router.svg`}
 />
+<RouterSymbol routerID={router.id} />
 <text x={router.node.x + 2} y={router.node.y + 10} dominant-baseline="central" font-size="smaller">
   {router.node.label}
 </text>
@@ -23,7 +25,7 @@
     text-anchor: middle;
   }
 
-  image {
+  svg {
     cursor: pointer;
   }
 </style>
